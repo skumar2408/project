d4aff92a0311bd0f024dcb7d3811690364b40d1e
Kumar <sameer.e.kumar@capgemini.com>
2019-11-02 17:39:39 +0530

    changes in the design and code

--- maven/hello-world/src/main/java/com/trainning/App.java
+++ maven/hello-world/src/main/java/com/trainning/App.java
@@ -4,10 +4,8 @@ package com.trainning;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+class App {
+    public static void main( final String[] args ) {
         System.out.println( "Hello World!" );
     }
 }
