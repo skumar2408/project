86e898b851092526352f1d2f7aba10ac4a77abd7
Kumar <sameer.e.kumar@capgemini.com>
2019-11-02 18:18:26 +0530

    changes made 10

--- maven/hello-world/src/main/java/com/trainning/App.java
+++ maven/hello-world/src/main/java/com/trainning/App.java
@@ -2,11 +2,16 @@ package com.trainning;
 
 /**
  * Hello world!
+ */
+final class App {
+    private App {
+
+    }
+/**
  * {@param final String[] args}
  * {@return} greeting message}
  */
-class App {
-    static void main(final String[] args) {
+    public static void main(final String[] args) {
         System.out.println("Hello World!");
     }
 }
